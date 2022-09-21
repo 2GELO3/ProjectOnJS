ef8050b7bbecbf3f6633ed9b62148192ba367d38
Oleg Vasilevsky <89103905+LEGO23@users.noreply.github.com>
2022-09-21 17:23:22 +0300

    Add files via upload

---
+++ ShoppingList/main.js
@@ -0,0 +1,28 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  if (input.value == '') {
+    button.disabled;
+  } else {
+    const myItem = input.value;
+    input.value = '';
+
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listItem.appendChild(listText);
+    listText.textContent = myItem;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = 'Delete';
+    list.appendChild(listItem);
+
+    listBtn.addEventListener('click', () => {
+      list.removeChild(listItem);
+    });
+
+    input.focus();
+  }
+});
